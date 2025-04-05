8eff5a1d790ae396242c60594cd67ac4c9bc92d2
Yonier Vasquez Marin <yoniervasquezmarin@gmail.com>
2025-04-05 17:43:20 -0500

    Categories list template is moved to external file

--- src/app/components/categories-list/categories-list.component.ts
+++ src/app/components/categories-list/categories-list.component.ts
@@ -4,34 +4,7 @@ import { CommonModule } from '@angular/common'
 
 @Component({
 	selector: 'app-categories-list',
-	template: `
-		<ion-header>
-			<ion-toolbar>
-				<ion-buttons slot="start">
-					<ion-back-button defaultHref="/home"></ion-back-button>
-				</ion-buttons>
-				<ion-title>Categorías</ion-title>
-			</ion-toolbar>
-		</ion-header>
-
-		<ion-content>
-			<ion-list>
-				<ion-item-group>
-					<ion-item-divider>
-						<ion-label>Mis Categorías</ion-label>
-					</ion-item-divider>
-
-					<!-- Aquí irá la lista de categorías -->
-					<ion-item>
-						<ion-label>Personal</ion-label>
-					</ion-item>
-					<ion-item>
-						<ion-label>Trabajo</ion-label>
-					</ion-item>
-				</ion-item-group>
-			</ion-list>
-		</ion-content>
-	`,
+	templateUrl: './categories-list.component.html',
 	standalone: true,
 	imports: [IonicModule, CommonModule],
 })
