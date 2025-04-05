888f5a16f01021c212ab002ea3ae64a0920b0e23
Yonier Vasquez Marin <yoniervasquezmarin@gmail.com>
2025-04-05 16:58:58 -0500

    Options button was added

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,19 +1,49 @@
-import { IonHeader, IonToolbar, IonTitle, IonContent, IonFab, IonFabButton, IonIcon } from '@ionic/angular/standalone'
+import {
+	IonHeader,
+	IonToolbar,
+	IonTitle,
+	IonContent,
+	IonFab,
+	IonFabButton,
+	IonIcon,
+	IonButtons,
+	IonButton,
+	IonPopover,
+	IonList,
+	IonItem,
+} from '@ionic/angular/standalone'
 import { TaskListComponent } from '../components/task-list/task-list.component'
+import { addOutline, ellipsisVertical, sparklesOutline, pricetagOutline } from 'ionicons/icons'
 import { Component, OnInit, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core'
-import { addOutline } from 'ionicons/icons'
 import { Router } from '@angular/router'
 
 @Component({
 	selector: 'app-home',
 	templateUrl: './home.component.html',
 	styleUrls: ['./home.component.scss'],
-	imports: [IonHeader, IonToolbar, IonTitle, IonContent, TaskListComponent, IonFab, IonFabButton, IonIcon],
+	imports: [
+		IonHeader,
+		IonToolbar,
+		IonTitle,
+		IonContent,
+		TaskListComponent,
+		IonFab,
+		IonFabButton,
+		IonIcon,
+		IonButtons,
+		IonButton,
+		IonPopover,
+		IonList,
+		IonItem,
+	],
 	standalone: true,
 	schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class HomeComponent implements OnInit {
 	addIcon = addOutline
+	menuIcon = ellipsisVertical
+	starIcon = sparklesOutline
+	tagIcon = pricetagOutline
 
 	constructor(private router: Router) {}
 
@@ -21,5 +51,13 @@ export class HomeComponent implements OnInit {
 
 	navigateToTaskDetail() {
 		this.router.navigate(['/task-detail'])
+	}
+
+	onAdvancedFeatures() {
+		console.log('Funciones adicionales seleccionada')
+	}
+
+	onCategories() {
+		console.log('Categorías seleccionada')
 	}
 }
