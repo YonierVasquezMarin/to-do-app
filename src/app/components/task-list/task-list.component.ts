a76bbc548e18773b029d86838d82660383eb1f9c
Yonier Vasquez Marin <yoniervasquezmarin@gmail.com>
2025-04-06 10:59:47 -0500

    Card added for show task

--- src/app/components/task-list/task-list.component.ts
+++ src/app/components/task-list/task-list.component.ts
@@ -1,16 +1,20 @@
-import { IonList, IonItem, IonLabel } from '@ionic/angular/standalone'
+import { IonIcon, IonCard, IonCardContent, IonText } from '@ionic/angular/standalone'
 import { TaskService } from '../../../services/task.service'
 import { Task } from '../../../models/business/task.model'
 import { Component, OnInit } from '@angular/core'
+import { libraryOutline } from 'ionicons/icons'
 import { CommonModule } from '@angular/common'
 
 @Component({
 	selector: 'app-task-list',
 	templateUrl: './task-list.component.html',
+	styleUrls: ['./task-list.component.scss'],
 	standalone: true,
-	imports: [CommonModule, IonList, IonItem, IonLabel],
+	imports: [CommonModule, IonIcon, IonCard, IonCardContent, IonText],
 })
 export class TaskListComponent implements OnInit {
+	libraryOutline = libraryOutline
+
 	// Array to store the tasks
 	tasks: Task[] = []
 
