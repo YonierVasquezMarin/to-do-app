64da0193938be524b1c0aa4adcbb89d9184a7d1d
Yonier Vasquez Marin <yoniervasquezmarin@gmail.com>
2025-04-06 22:07:26 -0500

    Feature sync is added

--- src/app/components/advanced-features/advanced-features.component.ts
+++ src/app/components/advanced-features/advanced-features.component.ts
@@ -1,9 +1,9 @@
 import { AdvancedFeatureService } from 'src/services/advanced-feature.service'
 import { AdvancedFeature } from 'src/models/business/advanced-features.model'
-import { IonicModule } from '@ionic/angular'
-import { Component } from '@angular/core'
-import { sparkles } from 'ionicons/icons'
+import { IonicModule, ToastController } from '@ionic/angular'
+import { sparkles, refreshCircle } from 'ionicons/icons'
 import { NgFor, NgClass } from '@angular/common'
+import { Component } from '@angular/core'
 
 @Component({
 	selector: 'app-advanced-features',
@@ -14,13 +14,33 @@ import { NgFor, NgClass } from '@angular/common'
 })
 export class AdvancedFeaturesComponent {
 	sparklesIcon = sparkles
+	refreshIcon = refreshCircle
 	features: AdvancedFeature[] = []
 
-	constructor(private advancedFeatureService: AdvancedFeatureService) {}
+	constructor(private advancedFeatureService: AdvancedFeatureService, private toastController: ToastController) {}
 
 	ngOnInit() {
 		this.advancedFeatureService.pipeAdvancedFeatures((features) => {
 			this.features = features
 		})
+	}
+
+	async updateFeatures() {
+		try {
+			await this.advancedFeatureService.updateFeatureStates()
+			this.showToast('Funciones actualizadas', 'success')
+		} catch (error) {
+			this.showToast('Error al actualizar las funciones', 'danger')
+		}
+	}
+
+	private async showToast(message: string, color: string) {
+		const toast = await this.toastController.create({
+			message,
+			duration: 2000,
+			position: 'bottom',
+			color,
+		})
+		await toast.present()
 	}
 }
